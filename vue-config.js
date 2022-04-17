c799e1c4cdb8662904526090db29fc836c3e9be4
sei-994 <s.hoshino0517basket@gmail.com>
2022-04-17 18:01:33 +0900

    init 2

--- vue-config.js
+++ vue-config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === "production" ? "/todays-best-clothes" : "/",
+  // baseUrl: process.env.NODE_ENV === "production" ? "/todays-best-clothes" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/weather-app/" //任意
+      : "/",
   outputDir: "docs",
 };
