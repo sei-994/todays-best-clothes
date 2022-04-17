8a7ccf257c45c3fafc6d8b419faedcf5618db36c
sei-994 <s.hoshino0517basket@gmail.com>
2022-04-17 17:58:04 +0900

    Updates

---
+++ precache-manifest.81fb8805265789c46efc42ddc09eb161.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8200a38c130a916a9c0f",
+    "url": "/css/app.3728e957.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4d564e26ca7edadc5f60c143ae6e059d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abed6c536871bcc3a197",
+    "url": "/js/about.f2eace00.js"
+  },
+  {
+    "revision": "8200a38c130a916a9c0f",
+    "url": "/js/app.8db78f0d.js"
+  },
+  {
+    "revision": "ecb841f3810514648fe4",
+    "url": "/js/chunk-vendors.808cf1d4.js"
+  },
+  {
+    "revision": "8097965b727bd641f39ceffb262132c8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
